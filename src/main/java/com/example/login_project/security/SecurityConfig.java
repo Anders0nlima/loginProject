ad0f3e35828ee0c41f7c8ee256854b8f509a274c
Anders0nlima <andersoncon85@gmail.com>
2025-02-22 10:18:09 -0300

    security

--- src/main/java/com/example/login_project/security/SecurityConfig.java
+++ src/main/java/com/example/login_project/security/SecurityConfig.java
@@ -2,11 +2,26 @@ package com.example.login_project.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/usuario", "/usuario/login").permitAll() // Permite acesso público ao registro e login
+                        .anyRequest().authenticated() // Exige autenticação para todos os outros endpoints
+                )
+                .csrf(csrf -> csrf.disable()); // Desabilita CSRF (útil para APIs REST)
+        return http.build();
+    }
+
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
